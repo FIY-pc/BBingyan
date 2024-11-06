561d18de19ed40a9cc8011a4144a69ad24cef9d4
FIY-pc <2953997758@qq.com>
2024-11-06 15:54:15 +0800

    feat(config): 配置读取

--- internal/config/readConfig.go
+++ internal/config/readConfig.go
@@ -1,1 +1,68 @@
 package config
+
+import (
+	"encoding/json"
+	"os"
+	"strings"
+)
+
+var Config configStruct
+
+type configStruct struct {
+	Server   ServerConfig   `json:"server"`
+	Postgres PostgresConfig `json:"postgres"`
+	Redis    RedisConfig    `json:"redis"`
+	Jwt      JwtConfig      `json:"jwt"`
+	Email    EmailConfig    `json:"email"`
+}
+
+type ServerConfig struct {
+	Port string `json:"port"`
+	Host string `json:"host"`
+}
+
+type PostgresConfig struct {
+	Dsn string `json:"dsn"`
+}
+
+type RedisConfig struct {
+	Addr     string `json:"addr"`
+	Password string `json:"password"`
+	DB       int    `json:"db"`
+}
+
+type JwtConfig struct {
+	Secret string `json:"secret"`
+	Expire int    `json:"expire"`
+}
+
+type EmailConfig struct {
+	SmtpUser     string `json:"smtp_user"`
+	SmtpPassword string `json:"smtp_password"`
+	SmtpNickname string `json:"smtp_nickname"`
+	SmtpHost     string `json:"smtp_host"`
+	SmtpPort     string `json:"smtp_port"`
+}
+
+type CaptchaConfig struct {
+	Length int `json:"length"`
+	Expire int `json:"expire"`
+}
+
+func InitConfig() {
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	parts := strings.Split(dir, "BBingyan")
+	path := parts[0] + "BBingyan/Config/default.json"
+
+	ConfigFile, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(ConfigFile, &Config)
+	if err != nil {
+		panic(err)
+	}
+}
