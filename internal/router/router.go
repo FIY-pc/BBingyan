8c345e54e5153f8b027efc7c3a8b4d745cbcf8fa
FIY-pc <2953997758@qq.com>
2024-11-06 15:53:10 +0800

    feat(*): 基本框架
    Closes: #5

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,1 +1,15 @@
 package router
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+func InitRouter(e *echo.Echo) {
+	InitPublicRouter(e)
+}
+
+func InitPublicRouter(e *echo.Echo) {
+	e.GET("/", func(c echo.Context) error {
+		return c.String(200, "Hello, World!")
+	})
+}
