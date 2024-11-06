a7f9f7ef76fdcf63b7af9ed89423663cc4130c24
FIY-pc <2953997758@qq.com>
2024-11-06 16:11:25 +0800

    feat(smtp): 基本6位验证码生成和smtp验证码邮件发送

---
+++ internal/util/smtp.go
@@ -0,0 +1,41 @@
+package util
+
+import (
+	"fmt"
+	"github.com/FIY-pc/BBingyan/internal/config"
+	"math/rand"
+	"net/smtp"
+	"time"
+)
+
+func SendCaptcha(address string, captcha string) error {
+	// 配置信息
+	user := config.Config.Email.SmtpUser
+	nickname := config.Config.Email.SmtpNickname
+	password := config.Config.Email.SmtpPassword
+	host := config.Config.Email.SmtpHost
+	port := config.Config.Email.SmtpPort
+	auth := smtp.PlainAuth("", user, password, host)
+	contentType := "Content-Type: text/plain; charset=UTF-8"
+	// 邮件标题
+	subject := "BBingyan验证码"
+	// 邮件主体
+	body := "您的验证码为：" + captcha + "\n该验证码有效期为5分钟，请尽快验证。"
+	// 邮件内容
+	rawMsg := fmt.Sprintf("To:%s\r\nFrom:%s<%s>\r\nSubject:%s\r\n%s\r\n\r\n%s", address, nickname, user, subject, contentType, body)
+	msg := []byte(rawMsg)
+	// 发送邮件
+	from := user
+	addr := fmt.Sprintf("%s:%s", host, port)
+	err := smtp.SendMail(addr, auth, from, []string{address}, msg)
+	return err
+}
+
+func GenerateCaptcha() string {
+	captcha := make([]byte, 6)
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < 6; i++ {
+		captcha[i] = byte(rand.Intn(10) + '0')
+	}
+	return string(captcha)
+}
