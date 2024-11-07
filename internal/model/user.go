202b6e3665577496c4edee86dab4900619904e42
FIY-pc <2953997758@qq.com>
2024-11-07 10:55:50 +0800

    feat: 邮箱验证码发送，登陆、注册，初始admin生成

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"github.com/FIY-pc/BBingyan/internal/config"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 	"time"
 )
@@ -16,7 +18,7 @@ type User struct {
 	Email      string    `json:"email" gorm:"unique"`
 	Nickname   string    `json:"nickname"`
 	Password   string    `json:"password"`
-	Permission uint      `json:"permission"` // 权限级别，普通用户为0，节点管理员为1，最高管理员为2
+	Permission int       `json:"permission" gorm:"default:1"` // 权限级别，普通用户为0，节点管理员为1，最高管理员为2
 	CreatedAt  time.Time `json:"created_at"`
 	UpdatedAt  time.Time `json:"updatedAt"`
 
@@ -74,4 +76,63 @@ func InitUser(db *gorm.DB) {
 	if err := db.AutoMigrate(&UserHistory{}); err != nil {
 		panic(err)
 	}
+}
+
+func InitSuperAdmin() {
+	var err error
+	_, err = GetUserByEmail(config.Config.User.Admin.Email)
+	if err != nil {
+		admin := &User{
+			Email:      config.Config.User.Admin.Email,
+			Password:   config.Config.User.Admin.Password,
+			Nickname:   "",
+			Permission: 10,
+		}
+		hashPassword, err := bcrypt.GenerateFromPassword([]byte(admin.Password), bcrypt.DefaultCost)
+		if err != nil {
+			panic(err)
+		}
+		admin.Password = string(hashPassword)
+		err = CreateUser(admin)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func CreateUser(user *User) error {
+	if err := postgresDb.Create(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetUserByEmail(email string) (*User, error) {
+	var user User
+	if err := postgresDb.Where("email =?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func GetUserByID(id uint) (*User, error) {
+	var user User
+	if err := postgresDb.Where("id =?", id).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func UpdateUser(user *User) error {
+	if err := postgresDb.Updates(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteUserByID(id uint) error {
+	if err := postgresDb.Delete(&User{}, id).Error; err != nil {
+		return err
+	}
+	return nil
 }
