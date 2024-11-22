af60d74216a9adf5eb3626d89b86fbb5e95bde8a
FIY-pc <2953997758@qq.com>
2024-11-22 16:51:46 +0800

    feat(controller): Add article CURD and comment CURD.

--- internal/controller/article.go
+++ internal/controller/article.go
@@ -1,1 +1,41 @@
 package controller
+
+import (
+	"github.com/FIY-pc/BBingyan/internal/controller/params"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func ArticleInfo(c echo.Context) error {
+
+	return c.JSON(http.StatusOK, params.Common200Resp{
+		Code: http.StatusOK,
+		Msg:  "Info article success",
+		Data: nil,
+	})
+}
+
+func ArticleCreate(c echo.Context) error {
+	return c.JSON(http.StatusOK, params.Common200Resp{
+		Code: http.StatusOK,
+		Msg:  "Create article success",
+		Data: nil,
+	})
+}
+
+func ArticleUpdate(c echo.Context) error {
+	return c.JSON(http.StatusOK, params.Common200Resp{
+		Code: http.StatusOK,
+		Msg:  "Update article success",
+		Data: nil,
+	})
+}
+
+func ArticleDelete(c echo.Context) error {
+
+	return c.JSON(http.StatusOK, params.Common200Resp{
+		Code: http.StatusOK,
+		Msg:  "Delete article success",
+		Data: nil,
+	})
+}
--- internal/model/article.go
+++ internal/model/article.go
@@ -13,10 +13,8 @@ type Article struct {
 	UpdatedAt  time.Time `json:"updated_at"`  // 文章更新时间
 	UserID     uint      `json:"user_id"`     // 文章作者的ID
 	Title      string    `json:"title"`       // 文章标题
-	LikeNum    uint      `json:"like_num"`    // 文章点赞数
 	CommentNum uint      `json:"comment_num"` // 文章评论数
 
-	Like    []Like    `json:"like" gorm:"foreignKey:ArticleID"`    // has many
 	Comment []Comment `json:"comment" gorm:"foreignKey:ArticleID"` // has many
 	Content Content   `json:"content" gorm:"foreignKey:ArticleID"` // has one
 }
@@ -27,14 +25,6 @@ type Content struct {
 	ArticleID uint   `json:"article_id"`
 	Text      string `json:"text" gorm:"type:text"`
 	UpdatedAt int64  `json:"updated_at"`
-}
-
-// Like 文章点赞表
-type Like struct {
-	ID        uint      `json:"id" gorm:"primarykey"`
-	ArticleID uint      `json:"article_id"` // 文章ID
-	UserID    uint      `json:"user_id"`    // 点赞用户的ID
-	CreatedAt time.Time `json:"created_at"` // 点赞时间
 }
 
 // Comment 文章评论表
@@ -51,9 +41,6 @@ func InitArticle(db *gorm.DB) {
 		panic(err)
 	}
 	if err := db.AutoMigrate(&Content{}); err != nil {
-		panic(err)
-	}
-	if err := db.AutoMigrate(&Like{}); err != nil {
 		panic(err)
 	}
 	if err := db.AutoMigrate(&Comment{}); err != nil {
@@ -131,8 +118,4 @@ func DeleteCommentByID(id uint) error {
 	}
 	postgresDb.Where("id", id).Delete(&Comment{})
 	return nil
-}
-
-func like() {
-
 }
