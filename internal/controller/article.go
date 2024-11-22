efd2a3e8548273fc38c44e4e888d8cfe25bfc2ea
FIY-pc <2953997758@qq.com>
2024-11-22 20:04:50 +0800

    feat(controller): Realize the Handlefuncs of "/articles" path.

--- internal/controller/article.go
+++ internal/controller/article.go
@@ -2,20 +2,48 @@ package controller
 
 import (
 	"github.com/FIY-pc/BBingyan/internal/controller/params"
+	"github.com/FIY-pc/BBingyan/internal/model"
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"strconv"
 )
 
 func ArticleInfo(c echo.Context) error {
-
+	rawArticleId := c.QueryParam("article_id")
+	ArticleId, err := strconv.Atoi(rawArticleId)
+	article, err := model.GetArticleByID(uint(ArticleId))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, params.CommonErrorResp{
+			Code:  http.StatusInternalServerError,
+			Msg:   "Get article failed",
+			Error: err.Error(),
+		})
+	}
 	return c.JSON(http.StatusOK, params.Common200Resp{
 		Code: http.StatusOK,
 		Msg:  "Info article success",
-		Data: nil,
+		Data: article,
 	})
 }
 
 func ArticleCreate(c echo.Context) error {
+	article := model.Article{}
+	err := c.Bind(&article)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, params.CommonErrorResp{
+			Code:  http.StatusInternalServerError,
+			Msg:   "Parse article failed",
+			Error: err.Error(),
+		})
+	}
+	err = model.CreateArticle(article)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, params.CommonErrorResp{
+			Code:  http.StatusInternalServerError,
+			Msg:   "Create article failed",
+			Error: err.Error(),
+		})
+	}
 	return c.JSON(http.StatusOK, params.Common200Resp{
 		Code: http.StatusOK,
 		Msg:  "Create article success",
@@ -24,6 +52,36 @@ func ArticleCreate(c echo.Context) error {
 }
 
 func ArticleUpdate(c echo.Context) error {
+	id, err := strconv.Atoi(c.FormValue("id"))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, params.CommonErrorResp{
+			Code:  http.StatusInternalServerError,
+			Msg:   "Parse id failed",
+			Error: err.Error(),
+		})
+	}
+	article, err := model.GetArticleByID(uint(id))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, params.CommonErrorResp{
+			Code:  http.StatusInternalServerError,
+			Msg:   "Parse article failed",
+			Error: err.Error(),
+		})
+	}
+	if title := c.QueryParam("title"); title != "" {
+		article.Title = title
+	}
+	if content := c.QueryParam("content"); content != "" {
+		article.Content.Text = content
+	}
+	err = model.UpdateArticle(*article)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, params.CommonErrorResp{
+			Code:  http.StatusInternalServerError,
+			Msg:   "Update article failed",
+			Error: err.Error(),
+		})
+	}
 	return c.JSON(http.StatusOK, params.Common200Resp{
 		Code: http.StatusOK,
 		Msg:  "Update article success",
@@ -32,7 +90,22 @@ func ArticleUpdate(c echo.Context) error {
 }
 
 func ArticleDelete(c echo.Context) error {
-
+	id, err := strconv.Atoi(c.FormValue("id"))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, params.CommonErrorResp{
+			Code:  http.StatusInternalServerError,
+			Msg:   "Parse id failed",
+			Error: err.Error(),
+		})
+	}
+	err = model.DeleteArticleByID(uint(id))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, params.CommonErrorResp{
+			Code:  http.StatusInternalServerError,
+			Msg:   "Delete article failed",
+			Error: err.Error(),
+		})
+	}
 	return c.JSON(http.StatusOK, params.Common200Resp{
 		Code: http.StatusOK,
 		Msg:  "Delete article success",
