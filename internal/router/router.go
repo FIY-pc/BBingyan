5237c63654a9db60f302387ba1d43b0167576dcc
FIY-pc <2953997758@qq.com>
2024-11-22 16:48:30 +0800

    feat(router): Add /user and /article paths.

--- internal/router/router.go
+++ internal/router/router.go
@@ -15,7 +15,20 @@ func InitPublicRouter(e *echo.Echo) {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(200, "Hello, BBingyan!")
 	})
+	// Public
 	e.GET("/captcha", controller.GetCaptcha)
 	e.POST("/tokens", controller.Login)
 	e.POST("/users/register", controller.Register)
+
+	// User
+	e.GET("/users", controller.UserInfo)
+	e.POST("/users/login", controller.Login)
+	e.PUT("/users", controller.UserUpdate)
+	e.DELETE("/users", controller.UserDelete)
+
+	// Article
+	e.GET("/articles", controller.ArticleInfo)
+	e.POST("/articles", controller.ArticleCreate)
+	e.PUT("/articles", controller.ArticleUpdate)
+	e.DELETE("/articles", controller.ArticleDelete)
 }
